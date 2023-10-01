1992e021313258cd55955713b0e73a71674976a8
Léonard Wojcik <leonardw@tutanota.com>
2023-10-01 20:02:29 +0200

    feat(server): merging the todoRouter

--- packages/server/src/routers/index.ts
+++ packages/server/src/routers/index.ts
@@ -1,6 +1,7 @@
 import { router } from "../config/trpc";
+import { todoRouter } from "./todoRouter";
 const appRouter = router({
-  // ...
+  todo: todoRouter,
 });
 // Export type router type signature,
 // NOT the router itself.
