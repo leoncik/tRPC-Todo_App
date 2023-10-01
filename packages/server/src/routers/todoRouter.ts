c932b8e23586b91a66804c3db92bf16341502fe6
Léonard Wojcik <leonardw@tutanota.com>
2023-10-01 19:58:41 +0200

    feat(server): add a mutation procedure

--- packages/server/src/routers/todoRouter.ts
+++ packages/server/src/routers/todoRouter.ts
@@ -1,8 +1,25 @@
 import { prisma } from "../config/prismaClient";
 import { publicProcedure, router } from "../config/trpc";
+import { z } from "zod";
 
 export const todoRouter = router({
   getAll: publicProcedure.query(() => {
     return prisma.todo.findMany();
   }),
+
+  create: publicProcedure
+    .input(
+      z.object({
+        content: z.string(),
+      })
+    )
+    .mutation((opts) => {
+      const { input } = opts;
+      return prisma.todo.create({
+        data: {
+          content: input.content,
+          isDone: false,
+        },
+      });
+    }),
 });
